8e02f3b0edc8fc2b53e1743e6992f800d807d671
Andrew <andrew.rutherford.6@gmail.com>
2020-04-14 10:46:20 +0700

    message is now an object with username, time, and text properties

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
@@ -10,22 +11,27 @@ const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = 'Shout Bot';
+
 // Run when a user connects
 io.on('connection', (socket) => {
   // Welcome current user
-  socket.emit('message', 'Welcome to Shout');
+  socket.emit('message', formatMessage(botName, 'Welcome to Shout'));
 
   // Broadcast when a user connects
-  socket.broadcast.emit('message', 'A user has joined the chat');
+  socket.broadcast.emit(
+    'message',
+    formatMessage(botName, 'A user has joined the chat')
+  );
 
   // Broadcast when a user disconnects
   socket.on('disconnect', () => {
-    io.emit('message', 'A user has left the chat');
+    io.emit('message', formatMessage(botName, 'A user has left the chat'));
   });
 
   // Listen for chatMessage
   socket.on('chatMessage', (msg) => {
-    io.emit('message', msg);
+    io.emit('message', formatMessage('USER', msg));
   });
 });
 
